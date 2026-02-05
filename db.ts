faa4dfefeb226c3078a869a28f1874e6c60d1d2c
kuryz <tj.frankiee@gmail.com>
2026-02-05 15:13:25 +0100

    update code

---
+++ db.ts
@@ -0,0 +1,12 @@
+import * as mysql from "mysql2/promise";
+import type { Pool } from "mysql2/promise";
+
+export const db: Pool = mysql.createPool({
+  host: process.env.DB_HOST ?? "localhost",
+  user: process.env.DB_USER ?? "root",
+  password: process.env.DB_PASSWORD ?? "",
+  database: process.env.DB_NAME ?? "test",
+  port: Number(process.env.DB_PORT ?? 3306),
+  waitForConnections: true,
+  connectionLimit: 10,
+});
